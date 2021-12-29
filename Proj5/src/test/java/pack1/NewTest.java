ba3ff6bc43653b0f7010dca095c530e5d57e6d5d
pruth <pruth@LAPTOP-I8AG8EMN>
2021-12-29 09:45:01 +0530

    Commit all

---
+++ Proj5/src/test/java/pack1/NewTest.java
@@ -0,0 +1,21 @@
+package pack1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void proj5() {
+	  
+	  System.out.println("Proj5");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
